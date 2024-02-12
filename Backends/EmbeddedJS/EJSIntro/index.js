3b2af5f133e6d139ba5de501ab1d3f955f089f98
Kumkum <kumkum.haz@gmail.com>
2024-02-12 23:56:28 +0530

    updated

--- Backends/EmbeddedJS/EJSIntro/index.js
+++ Backends/EmbeddedJS/EJSIntro/index.js
@@ -4,16 +4,23 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res ) => {
+    // const today = new Date("February 18, 2024 11:13:00");
     const today = new Date();
-    const dayOfWeek = today.getDay();
-    console.log(dayOfWeek); // Outputs a number between 0 and 6
+    const day = today.getDay();
+    console.log(day); // Outputs a number between 0 and 6
       let type = "a weekday";
       let adv = "it's time to work hard";
     
-      if(dayOfWeek === 0 || dayOfWeek === 6){
+      if(day === 0 || day === 6){
         type = "the weekend";
         adv = "it's time to have some fun";        
       }
+      // else{
+      //    type = "a weekday";
+      //    adv = "it's time to work hard";
+      // }
+
+
     res.render("index.ejs" , {
         dayType: type,
         advice: adv,
@@ -24,3 +31,4 @@ app.get("/", (req, res ) => {
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
 })
+
